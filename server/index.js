ee0db729e92d2b715d8d51af9dcc4ff0973f9860
https://github.com/denisphamzero7/DAN <denisphamzero7>
2024-06-06 01:14:49 +0700

    v2

--- server/index.js
+++ server/index.js
@@ -1,27 +1,36 @@
 const express = require('express');
-const initrouter= require('./routes/index');
-const cookieParser = require('cookie-parser')
-const dbconnect = require('./config/dbconnect')
-const corOptions = require('./config/cors')
+const initrouter = require('./routes/index');
+const cookieParser = require('cookie-parser');
+const dbconnect = require('./config/dbconnect');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT ||8888;
-//kết nối database mongo db
+const port = process.env.PORT || 8888;
+
+// Kết nối database MongoDB
 dbconnect();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use(morgan("common"));
-app.use(cors({
-    corOptions,
-    methods:['POST', 'PUT', 'DELETE','GET'],
-}));
-// đọc cookie
-app.use(cookieParser());
-initrouter(app)
+app.use(express.static('public'));  // Phục vụ tệp tĩnh từ thư mục 'public'
 
-app.listen(port,()=>{
-    console.log('running localhost:',port)
-})
+app.use(morgan('common'));
+app.use(cors({
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+}));
+
+// Đọc cookie
+app.use(cookieParser());
+
+// Xử lý yêu cầu favicon.ico
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+initrouter(app);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app; // Xuất module để Vercel có thể sử dụng
